9eb6326626ae6cb3617b83377f0fe4d48758aae5
Niranjan S <111201410+Ninja2k3@users.noreply.github.com>
2023-03-26 07:26:05 +0530

    almost finalized

--- site/app2.py
+++ site/app2.py
@@ -1,4 +1,20 @@
 from flask import Flask,render_template,Response,request,redirect,url_for
+import pyrebase
+
+config = {
+    "apiKey": "AIzaSyDybkQ-gyeyXoXZlF8fy26Y5JXo4svBq6Y",
+    "authDomain": "parking-management-4e32d.firebaseapp.com",
+    "projectId": "parking-management-4e32d",
+    "databaseURL": "https://parking-management-4e32d-default-rtdb.firebaseio.com/",
+    "storageBucket": "parking-management-4e32d.appspot.com",
+    "messagingSenderId": "1091771919954",
+    "appId": "1:1091771919954:web:1fa7d43eecdf4e6cdd798b",
+    "measurementId": "G-QQ1YN4MW68"
+}
+
+firebase=pyrebase.initialize_app(config)
+
+db=firebase.database()
 
 app=Flask(__name__)
 
@@ -8,11 +24,15 @@ app=Flask(__name__)
 def index():
     if request.method=='POST':
         location=request.form.get("fname1")
+        db.child("SPOT").push(location)
         ns=request.form.get("fname2")
+        db.child("SPOT").push(ns)
         cc=request.form.get("fname3")
+        db.child("SPOT").push(cc)
         cp=request.form.get("fname4")
+        db.child("SPOT").push(cp)
         linc=request.form.get("fname5")
-        locationdata=[location,ns,cc,cp,linc]
+        db.child("SPOT").push(linc)
         return render_template('6.html', fname1=location,fname2=ns,fname3=cc,fname4=cp,fname5=linc)
     else:
         return render_template('5.html')
@@ -39,7 +59,11 @@ def ownedupdated():
 
 @app.route('/parklocations')
 def parklocations():
-    return render_template('7.html')
+    cost=db.child('SPOT').get()
+    co=cost.val()
+    v=list(co.values())
+    v[4]=str(v[4])
+    return render_template('7.html',fname1=v[0],fname2=v[1],fname3=v[2],fname4=v[3],fname5=v[4])
 
 @app.route('/parking')
 def parking():
